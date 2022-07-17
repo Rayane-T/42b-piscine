eff3b856b86b8da8ee19582c360484955f8160f8
Rayane Tayache <82381064+Rayane-T@users.noreply.github.com>
2022-07-18 01:46:39 +0200

    C Piscine C03 - OK 100% (2022-06-29)

---
+++ C03/ft_strncmp.c
@@ -0,0 +1,15 @@
+int     ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+        int     i;
+
+        i = 0;
+        while ((s1[i] == s2[i]) && (s1[i] != '\0') && (s2[i] != '\0'))
+        {
+                if ((i+1) == n)
+                {
+                        break;
+                }
+                i++;
+        }
+        return (s1[i] - s2[i]);
+}
